d369e5b4ee3e0a61e69b65c4c8a5a8b5f86e26bf
martin mamdouh <martin.mamdouh2014@gmail.com>
2019-08-14 09:27:22 +0400

    remove client.py

--- onlineCalcProj/client/generate_token.py
+++ onlineCalcProj/client/generate_token.py
@@ -1,6 +1,6 @@
 import requests
 r = requests.post('http://127.0.0.1:7070/api/token/',
-                  data={"username": "admin", "password": "EWQ#ewq3"})
+                  data={"username": "user", "password": "megasoft123"})
 print(r.text)
 print(r.status_code)
 print(r.json())
